69255b0cacccc57edda0b909798efe1e8b88ac6a
Roxanne <cwillenbockel@aol.com>
2016-11-16 23:15:53 +0100

    V 1.6 EE

---
+++ Chloe/D/SoANew.d
@@ -0,0 +1,4 @@
+INTERJECT Bregg 0 RChloeBregg
+==RChloeJ IF~InParty("RChloe")~THEN~Maybe it's not important what your Mr F. likes. We like to remove scum from the streets and we don't like your faces at all.~
+==Bregg~Now ye've done it, blasted fool! Couldn't ye see the kensai girl was armed?~DO~Enemy()~
+END Cohrvale 1
